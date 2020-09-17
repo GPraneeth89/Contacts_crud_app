933bd94964c45bf3e9436482bbd81fe9fd667716
GPraneeth89 <gpraneeth89@gmail.com>
2020-09-17 14:30:34 +0530

    delete all

--- Contacts/settings.py
+++ Contacts/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$&d#0y+!vru^)+$#kis&qz_@@9bdp+l9d%t=3(y$hze^tidpe8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
